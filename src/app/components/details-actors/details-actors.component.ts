2b7ab8498b4e696a31ff8f243c0134bdc8535a5c
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2024-07-24 20:44:21 +0530

    Cleaned up using ngFor

--- src/app/components/details-actors/details-actors.component.ts
+++ src/app/components/details-actors/details-actors.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class DetailsActorsComponent {
 
+  actors: any[] = [
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 1', role: 'Role 1'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 2', role: 'Role 2'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 3', role: 'Role 3'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 4', role: 'Role 4'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 5', role: 'Role 5'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 6', role: 'Role 6'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 7', role: 'Role 7'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 8', role: 'Role 8'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 9', role: 'Role 9'},
+    {imageUrl:'../../../assets/images/shawshank3.jpg', name: 'Actor 10', role: 'Role 10'},
+  ]
+
 }
--- src/app/components/details-reviews/details-reviews.component.ts
+++ src/app/components/details-reviews/details-reviews.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './details-reviews.component.css'
 })
 export class DetailsReviewsComponent {
-
+  reviews: any[] = [
+    {
+      rate: '7.5/10',
+      review: 'This p tag is goint to have the review description. Sometimes it can be very long and that is why we are using accordion',
+      author: 'Review 1 author',
+      usefulCount: '100',
+      totalCount: '120'
+    },
+    {
+      rate: '8.5/10',
+      review: 'This p tag is goint to have the review description. Sometimes it can be very long and that is why we are using accordion',
+      author: 'Review 2 author',
+      usefulCount: '120',
+      totalCount: '150'
+    },
+    {
+      rate: '9.5/10',
+      review: 'This p tag is goint to have the review description. Sometimes it can be very long and that is why we are using accordion',
+      author: 'Review 3 author',
+      usefulCount: '150',
+      totalCount: '180'
+    },
+    
+  ]
 }
