7cca0b76c1a23586b2ba6f82b325870658e2419b
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2024-07-28 21:04:40 +0530

    Did the feedback form

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Feedback } from '../../data/model/Feedback';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
@@ -8,14 +9,24 @@ import { Feedback } from '../../data/model/Feedback';
 })
 export class FeedbackComponent implements OnInit{
 
-  feedback: Feedback = new Feedback('', '', 8, '', '');
+  // feedback: Feedback = new Feedback('', '', 8, '', '');
+  feedbackForm!: FormGroup
+
+  constructor(private fb: FormBuilder){}
 
   ngOnInit(): void {
-    
+    this.feedbackForm = this.fb.group({
+      emailAddress: [''],
+      phoneNumber: [''],
+      rate: [8],
+      feedbackTitle: [''],
+      feedback: ['']
+    })
   }
 
   submitFeedback(){
-    console.log("Feedback from [submit] - ", this.feedback);
+    // console.log("Feedback from [submit] - ", this.feedback);
+    console.log("Feedback form ", this.feedbackForm.value);
     
   }
 
